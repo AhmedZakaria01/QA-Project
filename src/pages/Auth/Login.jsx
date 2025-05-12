993cfd7662fd09705ad45395bd19d9edc75172ce
Ahmed Zakaria <64384776+AhmedZakaria01@users.noreply.github.com>
2025-05-12 14:11:58 +0300

    Login Form
    - Create Login Form
    - Install React Hook Form
    - integrate the Form with backend api to test it

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,28 +1,71 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../../features/auth/authSlice";
+import { useForm } from "react-hook-form";
+import SubmitButton from "../../components/SubmitButton";
 
 function Login() {
   const dispatch = useDispatch();
   const { user, status, error } = useSelector((state) => state.authReducer);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
+  const onSubmit = (data) => {
+    console.log(data);
+    dispatch(
+      login({
+        email: "marwsafouad22@gmail.com",
+        password: "Marwa@123",
+      })
+    );
+  };
+
+  console.log(watch("username")); // watch input value by passing the name of it
   return (
-    <section className="bg-red-600 p-4 text-white">
-      <button
-        type="button"
-        onClick={() =>
-          dispatch(
-            login({
-              email: "marwsafouad22@gmail.com",
-              password: "Marwa@123",
-            })
-          )
-        }
-        className="bg-white text-red-600 px-4 py-2 rounded"
-      >
-        Login
-      </button>
-    </section>
+    <form
+      className="bg-blue-950 text-white w-full max-w-md mx-auto mt-10 p-8 rounded-xl shadow-lg flex flex-col gap-y-4"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="flex flex-col w-full">
+        <label htmlFor="email" className="mb-1 text-sm font-medium">
+          Email
+        </label>
+        <input
+          className="p-2 border border-gray-300 rounded-md text-black focus:outline-none focus:ring-2 focus:ring-indigo-400"
+          name="email"
+          id="email"
+          placeholder="Enter your Email"
+          {...register("email")}
+        />
+      </div>
+
+      <div className="flex flex-col w-full">
+        <label htmlFor="password" className="mb-1 text-sm font-medium">
+          Password
+        </label>
+        <input
+          type="password"
+          className="p-2 border border-gray-300 rounded-md text-black focus:outline-none focus:ring-2 focus:ring-indigo-400"
+          name="password"
+          id="password"
+          placeholder="Enter your password"
+          {...register("password", { required: true })}
+        />
+        {errors.exampleRequired && (
+          <span className="text-red-400 mt-1 text-sm">
+            This field is required
+          </span>
+        )}
+      </div>
+
+      <div className="w-full ">
+        <SubmitButton bgColor="indigo-500" textColor="white" />
+      </div>
+    </form>
   );
 }
 

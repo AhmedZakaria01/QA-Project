ae1169798ac35a74baae5f0bb31940b62246080e
Ahmed Zakaria <64384776+AhmedZakaria01@users.noreply.github.com>
2025-06-30 16:08:29 +0300

    Structure Update
    1 - UseAxios Hook
    2- Localization
    3 - Folders - Files Structure
    4 - LocalStorage manager

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
-import { useDispatch } from "react-redux";
-import { login } from "./features/auth/authSlice";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ProtectedRoute from "./components/ProtectedRoute";
 import Login from "./pages/Auth/Login";
 import PageNotFound from "./pages/Auth/PageNotFound";
 import Layout from "./Layout";
 import Register from "./pages/Auth/Register";
-
 export default function App() {
-  const dispatch = useDispatch();
-
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />} />
 
+          {/* AUTH */}
           <Route path="/login" element={<Login />} />
-
           <Route path="/register" element={<Register />} />
 
           <Route path="*" element={<PageNotFound />} />
@@ -29,3 +23,5 @@ export default function App() {
 {
   /* <ProtectedRoute></ProtectedRoute> */
 }
+
+
